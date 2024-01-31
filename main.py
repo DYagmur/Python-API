0a79c78e5c955f9f5d945876afe1f6b19f02a4e0
Duygu Yagmur <30656517+DYagmur@users.noreply.github.com>
2024-01-31 13:31:21 -0800

    for loop

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def days_to_units(number_of_days):
 
 def validate_and_execute():
     try:
-        user_input_number  = int(user_input)
+        user_input_number  = int(num_of_days_element)
         if user_input_number > 0:
             calculated_value = days_to_units(user_input_number)
             print(calculated_value)
@@ -18,11 +18,14 @@ def validate_and_execute():
             print("You entered negative number. Please  enter a valid positive number.")
     except  ValueError:
         print ("Invalid input. Please enter a numeric value.")
-    
-while True:  
+        
+user_input = "" #assign an empty string to user_input
+while user_input != "exit":  #condition gets evaluated
     user_input = input("Hey user, enter a number of days and i will convert it to hours!\n")
-    validate_and_execute()
-
+    for num_of_days_element in user_input.split():
+        validate_and_execute()  #user is prompted for its input
+#function is called and input is validated and executed
+#second loop : check if exiit was typed in
 
 
 #conditional notes
@@ -32,3 +35,6 @@ while True:
 #otherwise:
 #do that ##### contition can be tue or false.
 #boolen data type have  two values True or False.
+
+
+#list --> [10, 20, 30, 40] 
