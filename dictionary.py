e45e8617531c7f3768996180361ccd0c0b039e98
Duygu Yagmur <30656517+DYagmur@users.noreply.github.com>
2024-02-03 23:32:23 -0800

    for dictionry

---
+++ dictionary.py
@@ -0,0 +1,15 @@
+#dictionary
+#are used to store values in key:value pairs
+#is a collection, which does not allow duplicate values
+
+# {"days": 20, "unit": "hours"}  #dictionary syntax  is curly braces {} and the items are separated by commas. Each item has a key can be accessed by the keys or index number
+
+
+
+
+
+my_list = ["20", "30", "100"]
+print(my_list[2])
+
+my_dictionary = {"days": 20, "unit": "hours", "message": "all good"}
+print(my_dictionary["message"])   
--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-calculation_to_units = 24
-name_of_unit = "hours"
+# calculation_to_units = 24
+# name_of_unit = "hours"
 
 
-def days_to_units(number_of_days):    
-        return f"{number_of_days} days are {number_of_days * calculation_to_units} {name_of_unit}"
-    
+def days_to_units(number_of_days, convertion_unit):    
+    if convertion_unit == "hours":
+        return f"{number_of_days} days are {number_of_days * 24} hours"
+    elif convertion_unit == "minutes":
+        return f"{number_of_days} days are {number_of_days * 24 * 60} minutes"
+    else:
+        return "Invalid unit for conversion. Please use 'hours' or 'minutes'"
 
 def validate_and_execute():
     try:
-        user_input_number  = int(num_of_days_element)
+        user_input_number  = int(days_and_unit_dictionary["days"])            #items can be accessed by its key name. you can use square brackets or get() method your_dict["days"] OR your_dict.get("days")
         if user_input_number > 0:
-            calculated_value = days_to_units(user_input_number)
+            calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"] )
             print(calculated_value)
         elif user_input_number == 0:
             print("you entered 0 . Please enter a valid positive number")
@@ -21,14 +25,40 @@ def validate_and_execute():
         
 user_input = "" #assign an empty string to user_input
 while user_input != "exit":  #condition gets evaluated
-    user_input = input("Hey user, enter a number of days and i will convert it to hours!\n")
-    list_of_days = user_input.split(", ")
-    print(list_of_days)
-    print(type(list_of_days))
-    print(set(list_of_days))
-    print(type(set(list_of_days)))
-    for num_of_days_element in list_of_days:
-        validate_and_execute()  #user is prompted for its input
+    user_input = input("Hey user, enter a number of days and conversation unit\n")
+    days_and_unit = user_input.split(":")
+    print(days_and_unit)
+    days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
+    print(days_and_unit_dictionary)
+    validate_and_execute()
+    
+
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+#      user_input = input("Hey user, enter a number of days and conversation unit\n")
+#     list_of_days = user_input.split(", ")
+#     print(list_of_days)
+#     print(type(list_of_days))
+#     print(set(list_of_days))
+#     print(type(set(list_of_days)))
+#     for num_of_days_element in list_of_days:
+#         validate_and_execute()   #user is prompted for its input
+        
+        
 #function is called and input is validated and executed
 #second loop : check if exiit was typed in
 
@@ -40,4 +70,14 @@ while user_input != "exit":  #condition gets evaluated
 #otherwise:
 #do that ##### contition can be tue or false.
 #boolen data type have  two values True or False.
+
+# print("some text") #prints to the standard output device
+# input("enter value") #asks user for input
+# set([1, 3, 4]) #returns a new set
+# int("20") # converts a value object into integer
+
+# "2, 3".split()
+# [1, 3, 4].count()
+
+
 
